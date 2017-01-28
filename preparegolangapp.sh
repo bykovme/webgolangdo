b70285b5f824606133298df76daf5fb7d1014106
Alex Bykov <alex@bykov.me>
2017-01-28 15:52:13 +0300

    changing config to non-interactive profile

--- preparegolangapp.sh
+++ preparegolangapp.sh
@@ -145,8 +145,8 @@ echo "$USERNAME:$PASSWORD" | chpasswd
 usermod -aG sudo $USERNAME
 
 runuser -l $USERNAME -c 'mkdir go'
-runuser -l $USERNAME -c 'printf "\nexport GOPATH=$HOME/go" >> ~/.bashrc'
-runuser -l $USERNAME -c 'printf "\nexport PATH=/usr/local/go/bin:$GOPATH/bin:$PATH\n" >> ~/.bashrc'
+runuser -l $USERNAME -c 'printf "\nGOPATH=$HOME/go" >> ~/.profile'
+runuser -l $USERNAME -c 'printf "\nPATH=/usr/local/go/bin:$GOPATH/bin:$PATH\n" >> ~/.profile'
 runuser -l $USERNAME -c 'cat ~/.bashrc'
 
 echo "Checking if GO was installed correctly..."
